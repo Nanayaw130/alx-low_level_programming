9c22e66348622ea04dc4421e21b7e780eb70a38f
nana <gyeduamankrah@gmail.com>
2022-09-12 18:30:04 +0000

    nana

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,9 +2,11 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
-int main(void) {
+int main(void)
+ {
     int n;
-    scanf("%d", &n);
+    srand(time(0));
+    n = rand() - RAND_MAX / 2;
     if(n > 0)
         printf("%i is positive\n", n);
     else if (n==0)
