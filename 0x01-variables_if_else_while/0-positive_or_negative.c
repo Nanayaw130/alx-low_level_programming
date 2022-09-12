f190f2ef5e8888d8fc263aecf9aae9b8fd30e145
nana <gyeduamankrah@gmail.com>
2022-09-12 18:18:39 +0000

    nana

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,12 +6,12 @@ int main(void) {
     int n;
     scanf("%d", &n);
     if(n > 0)
-        printf("%d is positive \n", n);
+        printf("%i is positive\n", n);
     else if (n==0)
-     printf("%d is zero \n", n);
+     printf("%i is zero\n", n);
     else
        {
-        printf("%d is  negative \n", n);
+        printf("%i is  negative\n", n);
        }
     return (0);
  } 
