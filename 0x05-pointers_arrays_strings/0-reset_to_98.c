5ced40c7bd2c36552191e9d323fdb2ea41995b46
Nanayaw130 <gyeduamankrah@gmail.com>
2022-09-19 18:11:49 +0000

    nana

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,24 +1,9 @@
-#include <stdio.h>
 /**
- * main - check the code
- *
- * Return: Always 0.
+ *updates the value to 98 
+ * 
  */
-void reset_to_98(int *n);
-int main(void)
-{
-    int n;
 
-    n = 402;
-    printf("n=%d\n", n);
-    reset_to_98(&n);
-    printf("n=%d\n", n);
-    return (0);
-
-}
 void reset_to_98(int *n)
 {
-*n =402;
-*n=98;
+	*n = 98;
 }
-
