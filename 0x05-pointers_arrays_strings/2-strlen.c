b743e8180dbc842c9fa8bcdfae990715173979e0
Nanayaw130 <gyeduamankrah@gmail.com>
2022-09-19 19:22:04 +0000

    nana

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,7 +3,8 @@
  * @s: pointer to the character array.
  * Return: length of the string
  */
-int _strlen(char * s) {
+int _strlen(char *s)
+ {
     char *p = s;
     int count = 0;
     while(*p != '\0')
