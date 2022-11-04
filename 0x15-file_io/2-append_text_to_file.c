60ad0851e0cd93717fa82e5074a598c7c2795ace
Nanayaw130 <gyeduamankrah@gmail.com>
2022-11-04 10:46:29 +0000

    nana

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,12 +10,12 @@
  * Return: 1 on success, -1 on failure
  */
 
-int append_text_to_file(const char *filename, char *text_content){
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int i = 0, file;
 
-	int i=0, file;
-	
 	if (filename == NULL)
-		return(-1);
+		return (-1);
 
 	if (text_content == NULL)
 		text_content = "";
